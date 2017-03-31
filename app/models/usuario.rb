b7fcb285f28a0978564373843dc80723e1818c47
Esteban <ejmeza@uc.cl>
2017-03-31 16:49:18 -0300

    Fix user controller

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,7 @@
 class Usuario < ApplicationRecord
   validates_presence_of :usuario, :nombre
+
+  def as_json(options={})
+    super(:except => [:created_at, :updated_at])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :usuarios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'usuario', to: 'usuarios#index'
+  get 'usuario/:id', to: 'usuarios#show'
+  delete 'usuario/:id', to: 'usuarios#destroy'
 end
